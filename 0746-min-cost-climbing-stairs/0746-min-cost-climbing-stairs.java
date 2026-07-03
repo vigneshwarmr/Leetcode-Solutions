2094b944f43c425da46cb152b4ace7dd2386d9be
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-03 15:40:38 +0530

    Time: 1 ms (39.57%), Space: 45.1 MB (23.17%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,15 +1,26 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
+
+class Solution{
+    public int minCostClimbingStairs(int[]cost){
         int n = cost.length;
         int[]dp = new int[n];
+        Arrays.fill(dp,-1);
 
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+        return Math.min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+    }
+    
+    private int solve(int i,int[]cost,int[]dp){
 
-        for(int i=2;i<n;i++){
-            dp[i]= cost[i]+Math.min(dp[i-1],dp[i-2]);
+        if(i==0)return cost[0];
+        if(i==1)return cost[1];
+
+        if(dp[i]!=-1){
+            return dp[i];
         }
 
-        return Math.min(dp[n-1],dp[n-2]);
+        dp[i]=cost[i]+Math.min(solve(i-1,cost,dp),solve(i-2,cost,dp));
+
+        return dp[i];
     }
 }
+
+
