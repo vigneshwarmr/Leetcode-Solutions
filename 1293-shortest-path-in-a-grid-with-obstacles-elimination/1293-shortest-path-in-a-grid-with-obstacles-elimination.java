538c6bca79d5f232efe235b00d31e4fcd43205ac
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-03 07:37:20 +0530

    Time: 24 ms (52.11%), Space: 47.7 MB (32.26%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,48 +1,51 @@
 class Solution {
-    int[]dr = {-1,1,0,0};
-    int[]dc ={0,0,-1,1};
     public int shortestPath(int[][] grid, int k) {
-        int m = grid[0].length;
         int n = grid.length;
+        int m = grid[0].length;
+
+        int[]dr = {-1,1,0,0};
+        int []dc ={0,0,-1,1};
 
         Queue<int[]> q = new LinkedList<>();
         boolean[][][]vis = new boolean[n][m][k+1];
+
         q.offer(new int[]{0,0,0,k});
         vis[0][0][k]=true;
-        
-        while(!q.isEmpty() ){
-            int size = q.size();
+
+        while(!q.isEmpty()){
+            int size =q.size();
 
             for(int i=0;i<size;i++){
-                int[]cell = q.poll();
+                int []cell = q.poll();
                 int x = cell[0];
-                int y = cell[1];
+                int y= cell[1];
                 int steps = cell[2];
                 int remainingK = cell[3];
 
-                if(x==n-1&& y==m-1) return steps;
+                if(x==n-1 && y==m-1)return steps;
 
                 for(int d=0;d<4;d++){
-                    int nr = x+ dr[d];
+                    int nr = x+dr[d];
                     int nc = y+ dc[d];
 
-                    if(nr<0|| nc<0 || nr>=n || nc>=m)continue;
+                    if(nr<0 || nc<0 || nr>=n || nc>=m)continue;
 
-                 
                     if(grid[nr][nc]==0){
                         if(!vis[nr][nc][remainingK]){
-                            q.offer(new int[]{nr,nc,steps+1,remainingK});
                             vis[nr][nc][remainingK]=true;
+                            q.offer(new int[]{nr,nc,steps+1,remainingK});
                         }
                     }else {
                         if(remainingK>0 && !vis[nr][nc][remainingK-1]){
-                        q.offer(new int[]{nr,nc,steps+1,remainingK-1});
-                        vis[nr][nc][remainingK-1]=true;
-                        }  
-                    } 
+                            vis[nr][nc][remainingK-1]=true;
+                            q.offer(new int[]{nr,nc,steps+1,remainingK-1});
+                        }
+                    }
+                    }
                 }
             }
-        }
+        
         return -1;
+
     }
 }
