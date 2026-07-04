8c91bd7dbff9380361b37a0a24e390c0ce01d510
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-04 11:43:31 +0530

    Time: 15 ms (9.46%), Space: 47.3 MB (7.15%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -3,30 +3,29 @@ class Solution {
         int n = edges.length;
         List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i =0;i<=n;i++){
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int []edge :edges){
-            int u = edge[0];
-            int v = edge[1];
-            boolean[]vis = new boolean[n+1];
-            
-            if(dfs(u,v,adj,vis))return edge;
+        for(int[]e :edges){
+            int u = e[0];
+            int v= e[1];
+            boolean []vis = new boolean[n+1];
+
+            if(dfs(u,v,adj,vis))return e;
 
             adj.get(u).add(v);
-            adj.get(v).add(u);  
+            adj.get(v).add(u);
         }
         return new int[0];
-
     }
 
-    private boolean dfs(int src,int dest,List<List<Integer>> adj ,boolean[]vis){
-
+    private boolean dfs(int src,int dest,List<List<Integer>>adj,boolean[]vis){
         if(src==dest)return true;
+
         vis[src]=true;
 
-        for(Integer x: adj.get(src)){
+        for(Integer x:adj.get(src)){
             if(!vis[x]){
                 if(dfs(x,dest,adj,vis))return true;
             }
