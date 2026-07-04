1e1f50393b4fd5bb9ce9917125a8b9c6abd99f9a
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-04 10:40:58 +0530

    Time: 4 ms (92.11%), Space: 47.1 MB (63.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean []vis = new boolean[numCourses];
-        boolean []pathVis = new boolean[numCourses];
+        int []vis = new int[numCourses];
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i=0;i<numCourses;i++){
@@ -15,25 +14,24 @@ class Solution {
         }
 
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i,prerequisites,adj,vis,pathVis))return false;
+            if(vis[i]==0){
+                if(!dfs(i,prerequisites,adj,vis))return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(int node,int[][]prerequisites,List<List<Integer>> adj,boolean[]vis,boolean[]pathVis){
+    private boolean dfs(int node,int[][]prerequisites,List<List<Integer>> adj,int[]vis){
 
-        vis[node]=true;
-        pathVis[node] = true;
+        vis[node]=1;
 
         for(Integer x: adj.get(node)){
-            if(!vis[x]){
-                if(dfs(x,prerequisites,adj,vis,pathVis))return true;
-            }else if(pathVis[x])return true;
+            if(vis[x]==0){
+                if(!dfs(x,prerequisites,adj,vis))return false;
+            }else if(vis[x]==1)return false;
         }
 
-        pathVis[node]=false;
-        return false;
+        vis[node]=2;
+        return true;
     }
 }
