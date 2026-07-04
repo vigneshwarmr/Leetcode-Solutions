e51d040e86fa1814f817836246e06de8bd310557
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-04 11:23:43 +0530

    Time: 15 ms (9.46%), Space: 47.3 MB (7.15%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i =0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int []edge :edges){
+            int u = edge[0];
+            int v = edge[1];
+            boolean[]vis = new boolean[n+1];
+            
+            if(dfs(u,v,adj,vis))return edge;
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);  
+        }
+        return new int[0];
+
+    }
+
+    private boolean dfs(int src,int dest,List<List<Integer>> adj ,boolean[]vis){
+
+        if(src==dest)return true;
+        vis[src]=true;
+
+        for(Integer x: adj.get(src)){
+            if(!vis[x]){
+                if(dfs(x,dest,adj,vis))return true;
+            }
+        }
+        return false;
+    }
+}
