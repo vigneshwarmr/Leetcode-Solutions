a3666c424b24eab5b3e212f663667ab6d8014356
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-05 13:21:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (22.04%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+        if(n==0)return 0;
         if(n==1)return nums[0];
 
         return Math.max(solve(0,n-2,nums),solve(1,n-1,nums));
+
     }
-
     private int solve(int start,int end,int[]nums){
+        int prev=0;
         int prev2=0;
-        int prev1=0;
 
-        for( int i=start;i<=end;i++){
+        for(int i=start;i<=end;i++){
             int pick= nums[i]+prev2;
-            int nopick = prev1;
-            int curr = Math.max(pick,nopick);
-            prev2=prev1;
-            prev1=curr;
+            int notpick= prev;
+            int curr= Math.max(pick,notpick);
+            prev2=prev;
+            prev=curr;
         }
-        return prev1;
+        return prev;
     }
 }
