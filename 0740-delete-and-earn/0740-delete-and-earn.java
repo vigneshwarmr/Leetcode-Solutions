96b574dbc5e522c264ff005626d697a791536cd7
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-05 23:36:02 +0530

    Time: 2 ms (99.86%), Space: 47.2 MB (22.82%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int max=0;
+        for(int x:nums){
+            max=Math.max(max,x);
+        }
+
+        int[]points = new int[max+1];
+
+        for(int x:nums){
+            points[x]+=x;
+        }
+
+        int prev2=0;
+        int prev=0;
+
+        for(int i=0;i<=max;i++){
+            int pick= points[i]+prev2;
+            int notpick= prev;
+            int curr = Math.max(pick,notpick);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+
+
+    }
+}
