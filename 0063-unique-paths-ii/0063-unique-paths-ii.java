38f1458b8732571cb5f9aa9b90507ce2bf0aea11
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-06 11:05:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    int[][]dp;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+
+        dp = new int[n][m];
+
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        return solve(0,0,obstacleGrid);
+    }
+
+    private int solve(int i,int j,int[][] obstacleGrid){
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+
+
+        if(i<0 || j<0 || i>=n || j>=n )return 0;
+        if(obstacleGrid[n-1][m-1]==1)return 0;
+        if(obstacleGrid[i][j]==1)return 0;
+
+        if(i==n-1 && j== m-1 )return 1;
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int ways= solve(i+1,j,obstacleGrid)+ solve(i,j+1,obstacleGrid);
+
+        return dp[i][j]=ways;
+
+    }
+}
