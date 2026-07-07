b92813f196ed7985096598835dd1d3417b5b3fd3
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-07 12:11:53 +0530

    Time: 11 ms (17.28%), Space: 161.8 MB (61.11%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
@@ -1,42 +1,38 @@
-class Solution {
+class Solution{
     int[][]dp;
-    public int minPathCost(int[][] grid, int[][] moveCost) {
+    public int minPathCost(int[][]grid,int[][]moveCost){
         int n = grid.length;
         int m = grid[0].length;
 
         dp = new int[n][m];
 
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             Arrays.fill(dp[i],-1);
         }
 
-        int ans =Integer.MAX_VALUE;
+        int ans=Integer.MAX_VALUE;
 
         for(int j=0;j<m;j++){
             ans = Math.min(ans,solve(grid,moveCost,0,j));
         }
         return ans;
+
+
     }
     private int solve(int[][]grid,int[][]moveCost,int row,int col){
         int n = grid.length;
         int m = grid[0].length;
 
-        if(row<0 || col<0 || row>=n || col>=m)return Integer.MAX_VALUE;
-
+        if(row<0 || col<0 || row>=n || col>=m){return Integer.MAX_VALUE;}
         if(row==n-1)return grid[row][col];
 
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-
+        if(dp[row][col]!=-1){return dp[row][col];}
         int ans = Integer.MAX_VALUE;
-
+        
         for(int nextCol=0;nextCol<m;nextCol++){
-            int path = grid[row][col]+ solve(grid,moveCost,row+1,nextCol)+moveCost[grid[row][col]][nextCol];
-            ans = Math.min(ans,path);
+            int min = grid[row][col]+ solve(grid,moveCost,row+1,nextCol)+moveCost[grid[row][col]][nextCol];
+            ans= Math.min(ans,min);
         }
-
-        return dp[row][col]=ans;
-
+        return dp[row][col] = ans;
     }
 }
