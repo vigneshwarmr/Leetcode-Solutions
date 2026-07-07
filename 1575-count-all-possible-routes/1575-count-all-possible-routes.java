79a015c82150bc22103392b59b61ee38269d7dd1
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-07 17:11:58 +0530

    Time: 56 ms (17.74%), Space: 46.4 MB (28.23%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -0,0 +1,33 @@
+class Solution {
+    int MOD =1_000_000_007;
+    Integer [][]dp;
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        dp = new Integer[locations.length][fuel+1];
+
+        return solve(locations,start,finish,fuel);
+    }
+
+    private int solve(int[] locations, int city, int finish, int fuel){
+
+        if(fuel<0)return 0;
+
+        if(dp[city][fuel]!=null){
+            return dp[city][fuel];
+        }
+
+        long ans = (city==finish)?1:0;
+
+        for(int next=0;next<locations.length;next++){
+
+            if(city==next)continue;
+
+            int cost =Math.abs(locations[city]-locations[next]);
+
+            if(fuel>=cost){
+                ans+=solve(locations,next,finish,fuel-cost);
+                ans%=MOD;
+            }
+        }
+        return dp[city][fuel]=(int)ans;
+    }
+}
