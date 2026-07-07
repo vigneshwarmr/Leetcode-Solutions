2641d1430b3aed1a427a30979c024b0ccd57d500
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-07 17:39:27 +0530

    Time: 47 ms (49.19%), Space: 46.3 MB (28.23%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,13 +1,13 @@
 class Solution {
-    int MOD =1_000_000_007;
-    Integer [][]dp;
+    int MOD = 1_000_000_007;
+    Integer[][]dp;
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        
         dp = new Integer[locations.length][fuel+1];
-
-        return solve(locations,start,finish,fuel);
+        return solve(locations, start, finish, fuel);
     }
 
-    private int solve(int[] locations, int city, int finish, int fuel){
+    public int solve(int[] locations, int city, int finish, int fuel) {
 
         if(fuel<0)return 0;
 
@@ -15,19 +15,19 @@ class Solution {
             return dp[city][fuel];
         }
 
-        long ans = (city==finish)?1:0;
+        long ans=(city==finish)?1:0;
 
         for(int next=0;next<locations.length;next++){
 
-            if(city==next)continue;
-
-            int cost =Math.abs(locations[city]-locations[next]);
+            if(next==city)continue;
+            int cost = Math.abs(locations[city]-locations[next]);
 
             if(fuel>=cost){
-                ans+=solve(locations,next,finish,fuel-cost);
+                ans +=solve(locations,next,finish,fuel-cost);
                 ans%=MOD;
             }
-        }
+       }
         return dp[city][fuel]=(int)ans;
+
     }
 }
