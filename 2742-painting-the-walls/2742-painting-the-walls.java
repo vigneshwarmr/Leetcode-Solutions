a07a956d88c2d9657215b61f00cf87a26e7e506d
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-07 13:58:54 +0530

    Time: 47 ms (30.65%), Space: 47.5 MB (60.48%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[][]dp;
+    int INF =(int)1e9;
+    public int paintWalls(int[] cost, int[] time) {
+        int n = cost.length;
+
+        dp = new int[n][n+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        return solve(cost,time,0,n);
+        
+    }
+    private int solve(int[]cost,int[]time,int i,int remain){
+
+        if(remain<=0)return 0;
+
+        if(i==cost.length)return INF;
+
+        if(dp[i][remain]!=-1){
+            return dp[i][remain];
+        }
+
+        int pay = cost[i]+ solve(cost,time,i+1,remain-(1+time[i]));
+        int skip= solve(cost,time,i+1,remain);
+
+        return dp[i][remain]= Math.min(pay,skip);
+    }
+}
