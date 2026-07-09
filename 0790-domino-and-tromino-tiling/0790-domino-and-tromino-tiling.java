8a0ebd81cdedb885af27bbd1893a796a1ef8a034
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-10 00:19:19 +0530

    Time: 1 ms (56.08%), Space: 42.3 MB (23.87%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -1,12 +1,12 @@
 class Solution {
-    int mod = 1_000_000_007;
+    int mod= 1_000_000_007;
     public int numTilings(int n) {
-        
+        int[]dp = new int[n+1];
+
         if(n<=1)return 1;
+        if(n==1)return 1;
         if(n==2)return 2;
         if(n==3)return 5;
-
-        int[]dp = new int[n+1];
 
         dp[0]=1;
         dp[1]=1;
@@ -14,7 +14,7 @@ class Solution {
         dp[3]=5;
 
         for(int i=4;i<=n;i++){
-            dp[i]= (int)((2L * dp[i-1]+ dp[i-3])%mod);
+            dp[i]= (int)((2L*dp[i-1]+dp[i-3])%mod);
         }
         return dp[n];
     }
