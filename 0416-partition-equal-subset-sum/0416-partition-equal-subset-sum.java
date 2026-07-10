9f3c3c7584eb5246ad1cd71cb50bdd0e7fb2ae5c
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-10 15:56:40 +0530

    Time: 94 ms (21.26%), Space: 69.5 MB (37.96%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    private boolean subset(int []nums,int sum){
+    private boolean solve(int[]nums,int sum){
         int n = nums.length;
-        boolean[][]dp= new boolean[n+1][sum+1];
+        Boolean[][]dp = new Boolean[n+1][sum+1];
 
         for(int i=0;i<=n;i++){
             for(int j=0;j<=sum;j++){
-                if(i ==0 && j==0){
-                    dp[i][j]=true;
-                }else if(j==0){
+                if( i==0 && j==0){
                     dp[i][j]=true;
                 }else if(i==0){
                     dp[i][j]=false;
+                }else if(j==0){
+                    dp[i][j]=true;
                 }
             }
         }
 
+
         for(int i=1;i<=n;i++){
             for(int j=0;j<=sum;j++){
                 if(nums[i-1]<=j){
-                    dp[i][j]= dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
                 }else{
                     dp[i][j]= dp[i-1][j];
                 }
@@ -26,16 +27,16 @@ class Solution {
         }
         return dp[n][sum];
     }
+
     public boolean canPartition(int[] nums) {
+        int sum=0;
         int n = nums.length;
-        int sum= 0;
-        
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
 
         if(sum%2!=0){
             return false;
-        }else return subset(nums,sum/2);
+        }else return solve(nums,sum/2);
     }
 }
