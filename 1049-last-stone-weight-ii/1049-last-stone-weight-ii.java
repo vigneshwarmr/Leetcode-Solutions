395ca32cfbe5181042af4bd4b6d69c02a69b07b5
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-11 17:13:37 +0530

    Time: 5 ms (30.4%), Space: 43.1 MB (54.36%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int n = stones.length;
+        int weight=0;
+        for(int num:stones){
+            weight+=num;
+        }
+
+        boolean [][]dp = new boolean[n+1][weight+1];
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=weight;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=true;
+                }else if(j==0){
+                    dp[i][j]=true;
+                }else if(i==0){
+                    dp[i][j]=false;
+                }
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=weight;j++){
+                if(stones[i-1]<=j){
+                   dp[i][j]= dp[i-1][j-stones[i-1]]||dp[i-1][j];
+                }else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int s1=0;s1<=weight/2;s1++){
+            if(dp[n][s1]){
+                min = Math.min(min,weight-2*s1);
+            }
+        }
+        return min;
+
+    }
+}
