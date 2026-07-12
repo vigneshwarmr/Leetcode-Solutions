b73d725b56e282dc3c3564aae9448eba088d4b10
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-12 17:03:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (46.41%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,25 +1,19 @@
 class Solution {
-    int count=0;
     Integer[]dp;
     public int combinationSum4(int[] nums, int target) {
-        int n = nums.length;
         dp = new Integer[target+1];
-    
-        return solve(nums,target);
+        return solve(target,nums);
     }
-    private int solve(int[]nums,int target){
-        if(target==0){
-            return 1;
-        }
-        if( target<0)return 0;
-   
+    private int solve(int target,int[]nums){
+        if(target==0)return 1;
+        if(target<0)return 0;
+
         if(dp[target]!=null)return dp[target];
         int ways=0;
 
         for(int num:nums){
-             ways +=solve(nums,target-num);
+            ways+=solve(target-num,nums);
         }
         return dp[target]=ways;
-
     }
 }
