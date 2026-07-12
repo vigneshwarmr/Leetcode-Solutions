67626172f66e6d94db64e1f87034ae060636c9c9
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-12 08:02:39 +0530

    Time: 7 ms (9.55%), Space: 44.6 MB (13.24%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,21 +1,23 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
+        
+        int weight =0;
         int n = stones.length;
-        int weight=0;
+
         for(int num:stones){
             weight+=num;
         }
 
-        boolean [][]dp = new boolean[n+1][weight+1];
+        Boolean [][]dp = new Boolean[n+1][weight+1];
 
         for(int i=0;i<=n;i++){
             for(int j=0;j<=weight;j++){
                 if(i==0 && j==0){
                     dp[i][j]=true;
+                }else if(i==0){
+                    dp[i][j]=false;;
                 }else if(j==0){
                     dp[i][j]=true;
-                }else if(i==0){
-                    dp[i][j]=false;
                 }
             }
         }
@@ -23,21 +25,21 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=0;j<=weight;j++){
                 if(stones[i-1]<=j){
-                   dp[i][j]= dp[i-1][j-stones[i-1]]||dp[i-1][j];
+                    dp[i][j]= dp[i-1][j-stones[i-1]] || dp[i-1][j];
                 }else{
                     dp[i][j]= dp[i-1][j];
                 }
             }
         }
 
-        int min = Integer.MAX_VALUE;
+        int min= Integer.MAX_VALUE;
 
-        for(int s1=0;s1<=weight/2;s1++){
+        for(int s1=0 ;s1<=weight/2;s1++){
             if(dp[n][s1]){
                 min = Math.min(min,weight-2*s1);
             }
         }
-        return min;
 
-    }
+        return min;
+}
 }
