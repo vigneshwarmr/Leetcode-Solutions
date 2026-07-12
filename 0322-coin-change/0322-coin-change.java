ccf4d0b586499cd3a569b4e8fb0442c55dd9b5b5
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-12 13:09:48 +0530

    Time: 23 ms (33.12%), Space: 47 MB (24.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -10,21 +10,22 @@ class Solution {
                 }else if(j==0){
                     dp[i][j]=0;
                 }else if(i==0){
-                    dp[i][j]=Integer.MAX_VALUE-1;;
+                    dp[i][j]=Integer.MAX_VALUE-1;
                 }
             }
         }
+
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
-                    dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
-                }else {
-                    dp[i][j]= dp[i-1][j];
+                    dp[i][j]= Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
 
-        return dp[n][amount] ==Integer.MAX_VALUE-1?-1:dp[n][amount];
+        return dp[n][amount]==Integer.MAX_VALUE-1?-1:dp[n][amount];
     }
 }
