3be6899497b5f643814f76471bb7aae05c9d5c02
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-24 15:47:48 +0530

    Time: 6 ms (87.17%), Space: 46.4 MB (80.62%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,55 +1,58 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        
         List<List<Integer>> adj = new ArrayList<>();
-        int[]indegree= new int[numCourses];
 
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int []edge :prerequisites){
+        int[]indegree = new int[numCourses];
+        for(int[]edge:prerequisites){
             int u = edge[0];
             int v = edge[1];
             adj.get(u).add(v);
             indegree[v]++;
         }
 
-        boolean[][]pre = new boolean[numCourses][numCourses];
-
-        Queue<Integer> q= new LinkedList<>();
-
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.offer(i);
             }
         }
+        
+
+        boolean[][]pre = new boolean[numCourses][numCourses];
 
         while(!q.isEmpty()){
-            int node = q.poll();
+            int node =q.poll();
 
             for(int nbr:adj.get(node)){
                 pre[nbr][node]=true;
-
                 for(int i=0;i<numCourses;i++){
                     if(pre[node][i]){
                         pre[nbr][i]=true;
                     }
                 }
+
                 indegree[nbr]--;
                 if(indegree[nbr]==0){
                     q.offer(nbr);
                 }
             }
-        }
+        }  
 
-        List<Boolean> ans = new ArrayList<>();
+        List<Boolean> res = new ArrayList<>();
 
-        for(int []qv:queries){
-            int u = qv[0];
-            int v = qv[1];
-            ans.add(pre[v][u]);
-        }
+        for(int[]qu:queries) {
+            int u = qu[0];
+            int v = qu[1];
 
-        return ans;
-    }
+            res.add(pre[v][u]);
+        } 
+
+        return res;   
+    
+}
 }
