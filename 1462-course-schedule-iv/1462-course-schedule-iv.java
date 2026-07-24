e6ca6f8fd19a112f7390904c4f713725bfe242c5
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-07-24 15:57:25 +0530

    Time: 5 ms (90.54%), Space: 46.1 MB (87.26%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -7,7 +7,8 @@ class Solution {
             adj.add(new ArrayList<>());
         }
 
-        int[]indegree = new int[numCourses];
+        int[]indegree= new int[numCourses];
+
         for(int[]edge:prerequisites){
             int u = edge[0];
             int v = edge[1];
@@ -16,17 +17,17 @@ class Solution {
         }
 
         Queue<Integer> q = new LinkedList<>();
+
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.offer(i);
             }
         }
-        
 
-        boolean[][]pre = new boolean[numCourses][numCourses];
+        boolean[][]pre = new boolean [numCourses][numCourses];
 
         while(!q.isEmpty()){
-            int node =q.poll();
+            int node = q.poll();
 
             for(int nbr:adj.get(node)){
                 pre[nbr][node]=true;
@@ -35,24 +36,23 @@ class Solution {
                         pre[nbr][i]=true;
                     }
                 }
-
                 indegree[nbr]--;
+
                 if(indegree[nbr]==0){
                     q.offer(nbr);
                 }
+
             }
-        }  
+        }
 
         List<Boolean> res = new ArrayList<>();
 
-        for(int[]qu:queries) {
+        for(int []qu: queries){
             int u = qu[0];
-            int v = qu[1];
-
+            int v= qu[1];
             res.add(pre[v][u]);
-        } 
+        }
 
-        return res;   
-    
-}
+        return res;
+    }
 }
