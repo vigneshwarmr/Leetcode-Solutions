770f199448a39b5cb432b182127d47ecd21385f1
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-08-12 18:08:44 +0530

    Time: 4 ms (91.11%), Space: 47.4 MB (21.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -6,39 +6,32 @@ class Solution {
         for(int i =0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        int []indegree = new int[numCourses];
+        int []vis = new int[numCourses];
 
         for(int[]edge : prerequisites){
             int u = edge[0];
             int v = edge[1];
             adj.get(u).add(v);
-            indegree[v]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> q = new Stack<>();
 
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.offer(i);
-            }
+            if(dfs(i,adj,vis))return false;
         }
 
-        List<Integer> res = new ArrayList<>();
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-            res.add(node);
-
-            for(int nbr:adj.get(node)){
-                indegree[nbr]--;
-
-                if(indegree[nbr]==0){
-                    q.offer(nbr);
-                }
-            }
-        }
-
-        if(res.size()!=numCourses) return false;
         return true;
+    }
+    private boolean dfs(int node,List<List<Integer>> adj,int[]vis){
+        vis[node]=1;
+
+        for(Integer x:adj.get(node)){
+            if(vis[x]==0){
+                if(dfs(x,adj,vis))return true;
+            }else if(vis[x]==1)return true;
+        }
+
+        vis[node]=2;
+        return false;
     }
 }
