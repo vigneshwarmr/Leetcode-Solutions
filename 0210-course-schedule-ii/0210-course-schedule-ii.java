b20eb3f97c94f86514f74b5b5b834b97d1d83044
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-08-14 16:26:40 +0530

    Time: 4 ms (94.02%), Space: 46.5 MB (91.11%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,51 +1,44 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
 
-        int[]indegree = new int[numCourses];
-
-        for(int []edge :prerequisites){
-            int u = edge[0];
-            int v = edge[1];
+        for(int []e :prerequisites){
+            int u = e[0];
+            int v= e[1];
             adj.get(v).add(u);
-            indegree[u]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
+        int[]vis = new int[numCourses];
+        Stack<Integer> st = new Stack<>();
 
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.offer(i);
+            if(vis[i]==0){
+                if(dfs(i,adj,vis,st))return new int[0];
             }
         }
-
-        List<Integer> res = new ArrayList<>();
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-            res.add(node);
-
-            for(int nbr:adj.get(node)){
-                indegree[nbr]--;
-                if(indegree[nbr]==0){
-                    q.offer(nbr);
-                }
-            } 
-        }
-
-        int[]ans = new int[numCourses];
-        if(res.size()!=numCourses)return new int[0];
-
-        for(int i=0;i<numCourses;i++){
-            ans[i]= res.get(i);
-        }
        
+        int indx =0;
+        int[]arr = new int[numCourses];
 
-        return ans;
+        while(!st.isEmpty()){
+            arr[indx++]=st.pop();
+        }
+        return arr;
+    }
+
+    private boolean dfs(int node ,List<List<Integer>>adj,int[]vis,Stack<Integer> st){
+        vis[node]=1;
+
+        for(Integer x:adj.get(node)){
+            if(vis[x]==0){
+                if(dfs(x,adj,vis,st))return true;
+            }else if(vis[x]==1)return true;
+        }
+        vis[node]=2;
+        st.push(node);
+        return false;
     }
 }
