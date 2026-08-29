6c033ed3b14faeb9cf39640468af38641c7b376a
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-08-29 21:58:18 +0530

    Time: 28 ms (23.39%), Space: 62.6 MB (44.82%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,28 +1,45 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         
-        List<Integer> res = new ArrayList<>();
-        int[]vis = new int[graph.length];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i =0;i<graph.length;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int[]indegree = new int[graph.length];
 
         for(int i=0;i<graph.length;i++){
-            if(!dfs(i,graph,vis)){
-                res.add(i);
+            for(Integer x:graph[i]){
+                adj.get(x).add(i);
+                indegree[i]++;
             }
         }
-        return res;
-    }
 
-    private boolean dfs(int node,int[][]graph,int[]vis){
+        Queue<Integer> q= new LinkedList<>();
 
-        vis[node]=1;
-
-        for(Integer x:graph[node]){
-            if(vis[x]==0){
-                if(dfs(x,graph,vis))return true;
-            }else if(vis[x]==1) return true;
+        for(int i =0;i<graph.length;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
         }
 
-        vis[node]=2;
-        return false;
+        List<Integer> res = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            res.add(node);
+
+            for(int nbr:adj.get(node)){
+                indegree[nbr]--;
+
+                if(indegree[nbr]==0){
+                    q.offer(nbr);
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    
     }
 }
