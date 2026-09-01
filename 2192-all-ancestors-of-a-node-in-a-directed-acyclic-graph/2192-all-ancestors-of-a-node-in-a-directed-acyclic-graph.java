46145025ac0657b8f4f7b7b9a370e724c36dc6ad
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-09-01 16:14:15 +0530

    Time: 55 ms (86.01%), Space: 80.8 MB (67.22%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -7,52 +7,34 @@ class Solution {
             adj.add(new ArrayList<>());
         }
 
-        int[]indegree = new int[n];
-
         for(int[]edge :edges){
             int u = edge[0];
             int v = edge[1];
             adj.get(u).add(v);
-            indegree[v]++;
         }
 
-        List<Set<Integer>> ancestors = new ArrayList<>();
+        List<List<Integer>> ancestors = new ArrayList<>();
 
         for(int i =0;i<n;i++){
-            ancestors.add(new HashSet<>());
+            ancestors.add(new ArrayList<>());
         }
-
-        Queue<Integer> q= new LinkedList<>();
-
+        
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.offer(i);
+            boolean []vis = new boolean[n];
+            dfs(i,i,adj,vis,ancestors);
+            
+        }
+        return ancestors;
+    }
+
+    private void dfs(int src,int node,List<List<Integer>> adj,boolean[]vis,List<List<Integer>> ancestors){
+        vis[node]=true;
+
+        for(int nbr:adj.get(node)){
+            if(!vis[nbr]){
+                ancestors.get(nbr).add(src);
+                dfs(src,nbr,adj,vis,ancestors);
             }
         }
-
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-
-            for(int nbr:adj.get(node)){
-                ancestors.get(nbr).add(node);
-                ancestors.get(nbr).addAll(ancestors.get(node));
-
-                indegree[nbr]--;
-                if(indegree[nbr]==0){
-                    q.offer(nbr);
-                }
-            }
-        }
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for(int i =0;i<n;i++){
-            List<Integer> list = new ArrayList<>(ancestors.get(i));
-            Collections.sort(list);
-            ans.add(list);
-        }
-
-        return ans;
     }
 }
