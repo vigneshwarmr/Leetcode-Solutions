af65abac572ec570756f7ed679191d1cb8f2e358
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-09-01 15:47:23 +0530

    Time: 83 ms (54.28%), Space: 100.2 MB (40.08%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+
+
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int[]indegree = new int[n];
+
+        for(int[]p : edges){
+            int u =p[0];
+            int v = p[1];
+            adj.get(u).add(v);
+            indegree[v]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+
+        List<Set<Integer>> ancestors = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            ancestors.add(new HashSet<>());
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int nbr:adj.get(node)){
+                ancestors.get(nbr).add(node);
+                ancestors.get(nbr).addAll(ancestors.get(node));
+                
+                indegree[nbr]--;
+
+                if(indegree[nbr]==0){
+                    q.offer(nbr);
+                }
+                }
+            }
+        
+
+        List<List<Integer >> res = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            List<Integer>list = new ArrayList<>(ancestors.get(i));
+            Collections.sort(list);
+            res.add(list);
+        }
+        return res;
+    }
+}
