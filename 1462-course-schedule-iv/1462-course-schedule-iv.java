87610152f32caa73f661373960b41932641bc700
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-09-01 15:17:27 +0530

    Time: 5 ms (77.9%), Space: 46.7 MB (48.07%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -2,34 +2,33 @@ class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         
         List<List<Integer>> adj = new ArrayList<>();
-        int[]indegree= new int[numCourses];
 
-
-        for(int i=0;i<numCourses;i++){
+        for(int i =0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int[]p :prerequisites){
+        int[]indegree= new int[numCourses];
+
+        for(int []p :prerequisites){
             int u = p[0];
-            int v= p[1];
+            int v = p[1];
             adj.get(v).add(u);
             indegree[u]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
-        boolean[][]pre = new boolean[numCourses][numCourses];
-
 
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.offer(i);
             }
         }
+        boolean[][]pre = new boolean[numCourses][numCourses];
 
         while(!q.isEmpty()){
             int node = q.poll();
             
-            for(int nbr:adj.get(node)){
+            for(int nbr :adj.get(node)){
                 pre[nbr][node]=true;
 
                 for(int i=0;i<numCourses;i++){
@@ -37,7 +36,6 @@ class Solution {
                         pre[nbr][i]=true;
                     }
                 }
-
                 indegree[nbr]--;
 
                 if(indegree[nbr]==0){
@@ -48,10 +46,10 @@ class Solution {
 
         List<Boolean> res = new ArrayList<>();
 
-        for(int []qu : queries){
-            int t = qu[0];
-            int v = qu[1];
-            res.add(pre[t][v]);
+        for(int []p :queries){
+            int a = p[0];
+            int b = p[1];
+            res.add(pre[a][b]);
         }
 
         return res;
