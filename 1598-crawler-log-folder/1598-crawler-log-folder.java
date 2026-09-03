4483bcd54ceeb06c1b00c2483dc9adbe449d0ffe
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-09-03 16:22:08 +0530

    Time: 1 ms (100%), Space: 43.3 MB (98.88%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -3,13 +3,10 @@ class Solution {
         
         int count=0;
 
-        for(String log:logs){
+        for(String log :logs){
             if(log.equals("../")){
-                if(count>0){
-                    count--;
-                }
-            }
-            else if(log.equals("./"))continue;
+                if(count>0)count--;
+            }else if(log.equals("./"))continue;
             else count++;
         }
         return count;
