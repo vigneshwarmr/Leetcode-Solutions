22011c16092c149edee96cef355776c1455bfa54
M.R.Vigneshwar <mrvigneshwar@icloud.com>
2026-09-03 15:37:38 +0530

    Time: 1 ms (98.33%), Space: 43.3 MB (67.72%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        
+        if(s.length()!=goal.length())return false;
+
+        String concat = s+s;
+
+        if(concat.contains(goal))return true;
+        else return false;
+    }
+}
